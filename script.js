9f98d6348236bd611000538acdc11ba76de3e65f
TeoArmanu <teoarmanu100@gmail.com>
2022-07-24 20:33:59 +0100

    Mobile Version

---
+++ script.js
@@ -0,0 +1,11 @@
+const boop = document.querySelector('.boop-img');
+const modal = document.querySelector('.boop-modal');
+const closeButton = document.querySelector('.close-btn');
+
+boop.addEventListener('click', ()=> {
+    modal.classList.add('is-open')
+
+    closeButton.addEventListener('click', ()=> {
+        modal.classList.remove('is-open')
+    })
+})
